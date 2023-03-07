3bb1a9d21e42f74d4ee3c92b199a54b828f54878
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-03-07 14:26:22 +0100

    seeder and migration

--- database/migrations/2023_03_07_130146_create_projects_table.php
+++ database/migrations/2023_03_07_130146_create_projects_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('proj_link');
+            $table->text('img');
+            $table->text('collab');
             $table->timestamps();
         });
     }
