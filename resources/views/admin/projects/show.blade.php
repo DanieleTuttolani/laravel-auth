4cf5928ee74f200fa364edcb8b0442da6fdfb445
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-03-08 13:26:23 +0100

    corrected typo

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,8 +2,8 @@
 
 
 @section('content')
-
-    <div class="mx-auto m-5 bg-secondary text-light p-3 rounded  " style="max-width: 700px">
+<div class="my-bgc vh-100 p-5">
+    <div class="mx-auto  bg-secondary text-light p-3 rounded" style="max-width: 700px">
         <div class="col">
             <p>
                 <strong>Nome: {{$project->title}}</strong>
@@ -22,5 +22,6 @@
             <img  class="img-fluid rounded" src="http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQkrjYxSfSHeCEA7hkPy8e2JphDsfFHZVKqx-3t37E4XKr-AT7DML8IwtwY0TnZsUcQ" alt="">
         </div>
     </div>
+</div>
     {{-- todo aggiungi i bottoni per aggiornare eliminare --}}
 @endsection
